5f0d8b310958c2326121553a4ff8421259585591
Natoandro <anatoandro@hotmail.com>
2022-05-18 02:55:46 +0300

    Display correct message status

--- app/features/chats/components/MessageView.tsx
+++ app/features/chats/components/MessageView.tsx
@@ -2,24 +2,26 @@ import clsx from 'clsx';
 import { HTMLAttributes } from 'react';
 import { MessageAttributes } from '~/db/models/messages';
 
+type MessageStatus = 'default' | 'sent' | 'seen';
+
 export interface MessageDisplayProps extends HTMLAttributes<HTMLDivElement> {
   message: MessageAttributes;
+  status: MessageStatus;
 }
 
-export default function MessageView({ message, className, ...props }: MessageDisplayProps) {
+export default function MessageView({ message, status, className, ...props }: MessageDisplayProps) {
   return (
     <div className={clsx('flex justify-end', className)} {...props}>
       <div className="w-20 grow" />
       <div className="grow-0 px-2 py-1 rounded-xl rounded-tr-none border bg-blue-500">
         <p className="whitespace-pre-wrap text-white">{message.text}</p>
       </div>
-      <MessageStatusView status="default" className="mx-2 my-1 self-end shrink-0" />
+      <MessageStatusView status={status} className="mx-2 my-1 self-end shrink-0" />
     </div>
   );
 }
 
 
-type MessageStatus = 'default' | 'sent' | 'seen';
 
 interface MessageStatusViewProps extends HTMLAttributes<HTMLDivElement> {
   status: MessageStatus;
--- app/features/chats/components/Messages.tsx
+++ app/features/chats/components/Messages.tsx
@@ -1,14 +1,10 @@
-import { HTMLAttributes, useEffect, useMemo, useRef } from 'react';
+import { Fragment, HTMLAttributes, useEffect, useRef } from 'react';
 import MessageView from './MessageView';
 import MessageInput from './MessageInput';
 import clsx from 'clsx';
-import { ChatAttributes } from '~/db/models/chats';
-import { useAppDispatch, useAppSelector } from '~/app/hooks';
-import { enqueueMessage, MessageMeta, messageSent, selectAllMessages, selectChat, selectChatState, selectIsReady, SendingStatus, setSendingStatus } from '~/app/features/chats/chatsSlice';
-import axios from 'axios';
-import { MessageAttributes } from '~/db/models/messages';
+import { useAppSelector } from '~/app/hooks';
+import { selectChatState, selectMessageLoadingStatus } from '@/chats/chatsSlice';
 import useFetchMessages from '../hooks/useFetchMessages';
-import { useCurrentUser } from '../../users/contexts/CurrentUser';
 import useActiveChatId from '../hooks/useActiveChatId';
 import MessageSender from './MessageSender';
 
@@ -20,9 +16,11 @@ export default function Messages({ className, ...props }: MessagesProps) {
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
   // TODO: const messagesAreReady = useAppSelector(...)
-  const allMessages = useAppSelector(s => selectAllMessages(s, activeChatId));
-  const isReady = allMessages != null;
-  // const isReady = useAppSelector(selectIsReady);
+  // const allMessages = useAppSelector(s => selectAllMessages(s, activeChatId));
+  // const isReady = allMessages != null;
+  const isReady = useAppSelector(
+    s => selectMessageLoadingStatus(s, activeChatId) === 'success'
+  );
 
   useFetchMessages(activeChatId);
 
@@ -35,12 +33,30 @@ export default function Messages({ className, ...props }: MessagesProps) {
     <div className={clsx('flex flex-col', className)} {...props}>
       <main className="grow flex flex-col space-y-2 py-2">
         <div className="grow" />
-        {isReady && allMessages.map(msg => (
-          <MessageView key={msg.id} message={msg} />
-        ))}
+        {isReady && (
+          <MessageList />
+        )}
       </main>
       <MessageInput ref={inputRef} />
       {isReady && <MessageSender chatId={activeChatId} />}
     </div>
+  );
+}
+
+function MessageList() {
+  const chatId = useActiveChatId();
+  const { messages, queue } = useAppSelector(s => selectChatState(s, chatId))!;
+  return (
+    <Fragment>
+      {messages.map(msg => (
+        <MessageView key={msg.id} message={msg} status="sent" />
+      ))}
+      {queue.map((msg, i, arr) => (
+        <MessageView
+          key={i} status="default"
+          message={{ ...msg, id: i - arr.length, createdAt: 0 }}
+        />
+      ))}
+    </Fragment>
   );
 }
