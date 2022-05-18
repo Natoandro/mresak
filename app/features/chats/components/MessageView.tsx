b4f4e245e514ed17f3cd7e4b4551e3774ce365c9
Natoandro <anatoandro@hotmail.com>
2022-05-18 10:35:08 +0300

    (message view): Use different styles by sender

--- app/features/chats/components/MessageView.tsx
+++ app/features/chats/components/MessageView.tsx
@@ -1,24 +1,50 @@
 import clsx from 'clsx';
-import { HTMLAttributes } from 'react';
+import { Fragment, HTMLAttributes } from 'react';
+import { useAppSelector } from '~/app/hooks';
 import { MessageAttributes } from '~/db/models/messages';
+import { selectCurrentUser } from '@/users/userSlice';
 
 type MessageStatus = 'default' | 'sent' | 'seen';
 
-export interface MessageDisplayProps extends HTMLAttributes<HTMLDivElement> {
+export interface MessageViewProps extends HTMLAttributes<HTMLDivElement> {
   message: MessageAttributes;
   status: MessageStatus;
 }
 
-export default function MessageView({ message, status, className, ...props }: MessageDisplayProps) {
+function MessageContainer({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={clsx('flex justify-end', className)} {...props}>
-      <div className="w-20 grow" />
-      <div className="grow-0 px-2 py-1 rounded-xl rounded-tr-none border bg-blue-500">
-        <p className="whitespace-pre-wrap text-white">{message.text}</p>
-      </div>
-      <MessageStatusView status={status} className="mx-2 my-1 self-end shrink-0" />
-    </div>
+    <div className={clsx('flex px-2', className)} {...props} />
   );
+}
+
+export default function MessageView({ message, status, ...props }: MessageViewProps) {
+  const user = useAppSelector(selectCurrentUser)!;
+  const sentByOther = message.senderId !== user.id;
+
+  const spacer = <div className="w-20 grow" />;
+  const boxClasses = "grow-0 px-2 py-1 rounded-xl";
+
+  if (sentByOther) {
+    return (
+      <MessageContainer {...props}>
+        <div className={clsx(boxClasses, 'rounded-tl-none bg-gray-200')}>
+          <p className="whitespace-pre-wrap text-gray-800">{message.text}</p>
+        </div>
+        {spacer}
+      </MessageContainer>
+    );
+  } else {
+    return (
+      <MessageContainer {...props}>
+        {spacer}
+        <div className={clsx(boxClasses, 'rounded-tr-none bg-blue-500')}>
+          <p className="whitespace-pre-wrap text-white">{message.text}</p>
+        </div>
+        <MessageStatusView status={status} className="ml-2 my-1 self-end shrink-0" />
+      </MessageContainer>
+    );
+  }
+
 }
 
 
